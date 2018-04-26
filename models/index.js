ea2be6629553fa00c78d2dbdbca600aa18973252
Abro1986 <AndrewBroestl@gmail.com>
2018-04-26 13:18:24 -0600

    maybe forgot to save the index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,10 @@
 let mongoose= require('mongoose')
+
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
 mongoose.connect('mongodb://localhost/fishing-spots');
+}
+
 module.exports.Spot = require("./spot.js")
 
